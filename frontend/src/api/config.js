94c3e1f1802be226d6e92b74a778385e721055eb
BalasimJasim <balasimboliewi@gmail.com>
2025-01-03 22:47:23 +0200

    fix

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,4 +1,5 @@
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+console.log("Environment:", import.meta.env.MODE);
 console.log("API_URL:", API_URL);
 
 export default API_URL;
